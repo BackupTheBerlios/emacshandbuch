11da8f9193ef720d21fe1d46b1dc3b224bd1b21a
spica <spica>
2004-04-09 14:30:42 +0000

    Tagebuchseite

---
+++ homepage/tagebuch.php
@@ -0,0 +1,17 @@
+<?php
+
+    $titel = "Highlights des Projektes";
+    
+    include "header.inc.php";
+    
+?>
+
+<h1>Tagebuch und Highlights</h1>
+
+<br>
+
+<?php
+
+    include "footer.inc.php";
+    
+?>    
