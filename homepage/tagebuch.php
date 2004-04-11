512e1f84cb4eb2acc4405c1baa5dced4e754d8d7
spica <spica>
2004-04-11 13:35:32 +0000

    Tagebucheintragung

--- homepage/tagebuch.php
+++ homepage/tagebuch.php
@@ -1,14 +1,26 @@
-<?php
+<?php<p></p>
 
-    $titel = "Highlights des Projektes";
+    $titel = "Tagebuch des Projektes";
     
     include "header.inc.php";
     
 ?>
 
-<h1>Tagebuch und Highlights</h1>
+<h1>Tagebuch</h1>
 
-<br>
+<p>
+<ul> <u>29.04.2004</u><br>
+	Anmeldung als Projekt bei Berlios</ul>
+	
+<ul> <u>31.03.2004</u><br>
+	Erstellung und Veröffentlichung einer ersten (provisorischen) Homepage auf www.emacs-handbuch.de</ul>
+	
+<ul> <u>08.04.2004</u><br>
+	Erste Veröffentlichung einer Projekt-Homepage unter http://emacshandbuch.berlios.de/</ul>		
+	
+
+</p>
+
 
 <?php
 
